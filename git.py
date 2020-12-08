5cf0f41eb6d876b2d612530bfb03fad35e8c4cce
Peter Ohaver <peteroh9@gmail.com>
2020-12-09 00:17:49 +0100

    GitHub pusher

--- git.py
+++ git.py
@@ -5,6 +5,7 @@ filename = input("What is the filename? ")
 s.run(["git", "add", filename])
 
 comments = input("What comments do you have? ")
+comments.split()
 
 s.run(["git", "commit", "-m", comments])
 
