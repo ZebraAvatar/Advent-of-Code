c19f7d30ea57b93bffbf5d0b9f9dd5cc5335d0ad
Peter Ohaver <peteroh9@gmail.com>
2020-12-09 00:16:58 +0100

    GitHub pusher

---
+++ git.py
@@ -0,0 +1,13 @@
+import subprocess as s
+
+filename = input("What is the filename? ")
+
+s.run(["git", "add", filename])
+
+comments = input("What comments do you have? ")
+
+s.run(["git", "commit", "-m", comments])
+
+s.run("git", "pull")
+
+s.run("git", "push", "origin", "main")
